616e440f3b1e05aaec6f06ff623bc44e8c730054
Sandeep1061994 <sandeep.tiwari@groww.in>
2019-06-28 14:22:41 +0530

    webview updated

--- react-native-highcharts.js
+++ react-native-highcharts.js
@@ -1,21 +1,19 @@
-import React, { Component, PropTypes, } from 'react';
+import React, { Component } from 'react';
 import {
-    AppRegistry,
     StyleSheet,
-    Text,
     View,
-    WebView,
-    Image,
     Dimensions
 } from 'react-native';
+import { WebView } from 'react-native-webview';
+
 
 const win = Dimensions.get('window');
 class ChartWeb extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        this.state={
-            init:`<html>
+        this.state = {
+            init: `<html>
                     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
                     <style media="screen" type="text/css">
                     #container {
@@ -33,17 +31,17 @@ class ChartWeb extends Component {
                     <head>
                         <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
                         ${this.props.stock ? '<script src="https://code.highcharts.com/stock/highstock.js"></script>'
-                                      : '<script src="https://code.highcharts.com/highcharts.js"></script>'}
+                    : '<script src="https://code.highcharts.com/highcharts.js"></script>'}
                         ${this.props.more ? '<script src="https://code.highcharts.com/highcharts-more.js"></script>'
-                                      : ''}
+                    : ''}
                         ${this.props.guage ? '<script src="https://code.highcharts.com/modules/solid-gauge.js"></script>'
-                                      : ''}
+                    : ''}
                         <script src="https://code.highcharts.com/modules/exporting.js"></script>
                         <script>
                         $(function () {
                             Highcharts.setOptions(${JSON.stringify(this.props.options)});
                             Highcharts.${this.props.stock ? 'stockChart' : 'chart'}('container', `,
-            end:`           );
+            end: `           );
                         });
                         </script>
                     </head>
@@ -52,9 +50,9 @@ class ChartWeb extends Component {
                         </div>
                     </body>
                 </html>`,
-            Wlayout:{
-                height:win.height,
-                width:win.width
+            Wlayout: {
+                height: win.height,
+                width: win.width
             }
         }
     }
@@ -75,47 +73,47 @@ class ChartWeb extends Component {
 
         config = JSON.parse(config)
         let concatHTML = `${this.state.init}${flattenObject(config)}${this.state.end}`;
-        
+
         return (
-          <View style={this.props.style}>
-              <WebView
-                  onLayout={this.reRenderWebView}
-                  style={styles.full}
-                  source={{ html: concatHTML, baseUrl: 'web/' }}
-                  javaScriptEnabled={true}
-                  domStorageEnabled={true}
-                  scalesPageToFit={true}
-                  scrollEnabled={false}
-                  automaticallyAdjustContentInsets={true}
-                  {...this.props}
-              />
-          </View>
+            <View style={this.props.style}>
+                <WebView
+                    onLayout={this.reRenderWebView}
+                    style={styles.full}
+                    source={{ html: concatHTML, baseUrl: 'web/' }}
+                    javaScriptEnabled={true}
+                    domStorageEnabled={true}
+                    scalesPageToFit={true}
+                    scrollEnabled={false}
+                    automaticallyAdjustContentInsets={true}
+                    {...this.props}
+                />
+            </View>
         );
     };
 };
 
-var flattenObject = function (obj, str='{') {
-    Object.keys(obj).forEach(function(key) {
+var flattenObject = function (obj, str = '{') {
+    Object.keys(obj).forEach(function (key) {
         str += `${key}: ${flattenText(obj[key])}, `
     })
     return `${str.slice(0, str.length - 2)}}`
 };
 
-var flattenText = function(item,key) {
-    if(key=="y") console.log(item, typeof item);
+var flattenText = function (item, key) {
+    if (key == "y") console.log(item, typeof item);
     var str = ''
     if (item && typeof item === 'object' && item.length == undefined) {
         str += flattenObject(item)
     } else if (item && typeof item === 'object' && item.length !== undefined) {
         str += '['
-        item.forEach(function(k2) {
+        item.forEach(function (k2) {
             str += `${flattenText(k2)}, `
         })
-        if(item.length>0) str = str.slice(0, str.length - 2)
+        if (item.length > 0) str = str.slice(0, str.length - 2)
         str += ']'
-    } else if(typeof item === 'string' && item.slice(0, 8) === 'function') {
+    } else if (typeof item === 'string' && item.slice(0, 8) === 'function') {
         str += `${item}`
-    } else if(typeof item === 'string') {
+    } else if (typeof item === 'string') {
         str += `\"${item.replace(/"/g, '\\"')}\"`
     } else {
         str += `${item}`
@@ -125,8 +123,8 @@ var flattenText = function(item,key) {
 
 var styles = StyleSheet.create({
     full: {
-        flex:1,
-        backgroundColor:'transparent'
+        flex: 1,
+        backgroundColor: 'transparent'
     }
 });
 
